c3e4a222d4d827807b2a8b0c58e7667a6b52ceff
juno-choi <ililil9482@naver.com>
2022-02-25 15:32:43 +0900

    vue project 시작

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
