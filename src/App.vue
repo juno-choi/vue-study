e28103397986affe515373529e0900b901a9b803
juno-choi <ililil9482@naver.com>
2022-02-27 12:37:18 +0900

    Vue 이해하기 [4] method

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <h2 v-for="(user, idx) in users" :key="idx">이름 : {{ user.name }} 직업 : {{ user.job }} - {{ idx }}
-      <ul>
-        <li v-for="(o, idx) in user.hobby" :key="idx">{{ o }}</li>
-      </ul>
-    </h2>
+    <h2>count = {{ count }}</h2>
+    <button @click="addCount">증가</button>
   </div>
 </template>
 
@@ -13,8 +10,12 @@ export default {
   name: 'App',
   data(){
     return {
-      arr : ["obj1","obj2","obj3"],
-      users : [{name:'tester', job:'programmer', hobby:['축구','농구','야구']},{name:'tester2', job:'doctoer', hobby:['책읽기','드라이브','여행']}]
+      count : 0
+    }
+  },
+  methods:{
+    addCount(){
+      this.count++;
     }
   }
 }
